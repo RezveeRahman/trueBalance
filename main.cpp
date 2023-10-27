cf8f4831d574359b772a20b6fe7c46cbd7a69313
Bishoy <bsargius@gmail.com>
2023-10-26 22:23:54 -0400

    Updated main welcome printout and suppressed make file outputs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 int main(int argc, char* argv[]) {
-    std::cout << "Hello World!\n";
+    std::cout << "Welcome to True Balance!\n Here we make your life easier by "
+        << "ensuring that you are able to buy the things you want without "
+        << "over spending and missing out on rent or other important bills.\n";
     return 0;
 }
